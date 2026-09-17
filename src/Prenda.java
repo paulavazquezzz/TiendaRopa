054d4a004cef911def4f42f383ab68774a16f062
paulavazquezzz <paula.vazquez@alumnos.upm.es>
2026-09-17 11:36:19 +0200

    Creada clase Prenda con atributos y constructor

---
+++ src/Prenda.java
@@ -0,0 +1,16 @@
+
+public class Prenda {
+ private String modelo;
+ private String color;
+ private String talla;
+ private double precio;
+ private int stock;
+ 
+ public Prenda(String modelo,String color,String talla, double precio,int stock) {
+	 this.modelo=modelo;
+	 this.color=color;
+	 this.talla=talla;
+	 this.precio=precio;
+	 this.stock=stock;
+ }
+}
